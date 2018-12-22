4b505a24037812d40065a6ea9845888a59aa3a74
Cloudnextgeneration <info@cloudnextbd.com>
2018-12-23 00:35:20 +0600

    closures

---
+++ src/closures.js
@@ -0,0 +1,11 @@
+function makeFunc() {
+  var name = 'Mozilla';
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+var myFunc = makeFunc();
+myFunc();
+console.log(makeFunc());
